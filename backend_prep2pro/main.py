56e5516adf3564f9a0708aec11ab3c0d3c1e2600
ArunkumarCIT2027 <arunkumarm.cse2023@citchennai.net>
2025-01-08 13:27:59 +0530

    Is it deploy ready? please TwT

--- backend_prep2pro/main.py
+++ backend_prep2pro/main.py
@@ -1,6 +1,10 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False)  # Set debug=False in production
+    # Fetch the port from the environment variable or default to 5000 for local development
+    port = int(os.environ.get("PORT", 5000))
+    # Run the app on 0.0.0.0 and the specified port
+    app.run(host="0.0.0.0", port=port, debug=False)
